22484edf018e19182c9073b959a2e8cd557cb48f
thomasferew <58610700+thomasferew@users.noreply.github.com>
2020-02-27 13:47:29 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+// write out a list of your five favorite foods here:
+
+// 1. pasta
+
+let food1 = "pasta"
+
+// 2. mac and cheese
+
+let food2 = "mac and cheese"
+
+// 3. chicken salad
+
+let food3 = "chicken salad"
+
+// 4. chicken
+
+let food4 = "chicken"
+
+// 5. eggs
+
+let food5 = "eggs"
+
+// display all five foods in a single array
+
+let listfoods = ["pasta", "mac and cheese", "chicken salad", "chicken", "eggs"]
+
+game.splash("this is my first favorite food: pasta " + listfoods[0])
+
+game.splash("this is my third favorite food: chicken salad "[3])
+
+game.splash("this is my fifth favorite food: eggs "[5])
+
